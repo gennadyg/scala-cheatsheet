7f4e369cba7afb9c595d6fc2def7092f7885040b
Gennady.Gilin <gennady.gilin@clicktale.com>
2018-03-05 05:17:49 +0200

    Scala

--- Bounds/Wildcards.scala
+++ Bounds/Wildcards.scala
@@ -1,6 +1,6 @@
 
 /*
- * Copyright (c) 2015-2017 Toby Weston
+ * Copyright (c) 2015-2017 Gennady Gilin
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
